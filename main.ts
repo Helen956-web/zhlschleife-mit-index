e5f1b2fa2ee3c158bb556e77647e1ae6cb5d6760
Helen956-web <helen.mueller@thg-hn.de>
2024-10-17 10:33:02 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+for (let Index = 0; Index <= 9; Index++) {
+    basic.showNumber(Index + 1, 50)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
